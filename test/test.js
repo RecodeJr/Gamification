d2561e96ae87e4a9a6fe180271d93b32d31fef32
joshuapassos <joshuapassos@gmail.com>
2017-03-19 18:20:43 -0300

    fix

--- test/test.js
+++ test/test.js
@@ -5,8 +5,6 @@ var base_url = "http://localhost:5000/"
 describe("Gamification", function() {
 
 
-
-
   describe("GET /sexo", function() {
     it("returns status code 200", function(done) {
       request.get(base_url + "sexo", function(err, res, body) {
