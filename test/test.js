f87df4fbac73cd1d482d73bd006efb788cf8af7f
joshuapassos <joshuapassos@gmail.com>
2017-03-19 18:13:59 -0300

    add unit test

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var request = require("request");
+var expect = require("expect.js")
+var base_url = "http://localhost:5000/"
+
+describe("Gamification", function() {
+
+
+
+
+  describe("GET /sexo", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url + "sexo", function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+
+
+
+});
